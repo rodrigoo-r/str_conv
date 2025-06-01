7adc4543da7b7839ff9a10fa40637071d988f714
rodrigoo-r <2nw53j32643y@gmail.com>
2025-05-31 21:21:05 -0500

    Add parse_hex4 function to library.h for hexadecimal string parsing

--- library.h
+++ library.h
@@ -11,4 +11,25 @@
 #ifndef FLUENT_LIBC_STR_CONV_LIBRARY_H
 #define FLUENT_LIBC_STR_CONV_LIBRARY_H
 
+/**
+ * Parses a 4-character hexadecimal string and returns its integer value.
+ *
+ * \param str Pointer to a string containing at least 4 hexadecimal characters.
+ * \return The integer value of the 4 hex digits, or -1 if an invalid character is found.
+ *
+ * Example: parse_hex4("1a2F") returns 6703.
+ */
+static inline int parse_hex4(const char *str) {
+    int val = 0;
+    for (int i = 0; i < 4; i++) {
+        const char c = str[i];
+        val <<= 4; // Shift left by 4 bits to make room for the next hex digit
+        if      ('0' <= c && c <= '9') val |= (c - '0');         // Handle numeric digits
+        else if ('a' <= c && c <= 'f') val |= (c - 'a' + 10);    // Handle lowercase hex letters
+        else if ('A' <= c && c <= 'F') val |= (c - 'A' + 10);    // Handle uppercase hex letters
+        else return -1; // invalid hex character found
+    }
+    return val;
+}
+
 #endif //FLUENT_LIBC_STR_CONV_LIBRARY_H
